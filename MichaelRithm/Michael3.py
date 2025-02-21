da854a86cebc54dcfd4389ae370324ab29d2f123
William <wangwilli1bsd@gmail.com>
2025-02-21 05:53:43 -0800

    Bitwise Mask is not working

---
+++ MichaelRithm/Michael3.py
@@ -0,0 +1,138 @@
+import cv2
+import numpy as np
+
+asdf = []
+
+
+img = cv2.imread("imgs/img1.png")
+# make the img actually visible
+img = cv2.resize(img, (600, 800), img)
+
+# --- BLUE CONSTANTS ---
+#lowHSV = np.array([0, 60, 30])
+#highHSV = np.array([80, 255, 255])
+
+# --- RED CONSTANTS ---
+#lowHSV = np.array([115, 60, 80])
+#highHSV = np.array([180, 255, 255])
+
+# --- YELLOW CONSTANTS ---
+lowHSV = np.array([90, 150, 100])
+highHSV = np.array([110, 255, 255])
+
+
+
+CONTOUR_THRESH_LOW = 500
+CONTOUR_THRESH_HIGH = 4000
+
+
+dim = (600, 800)
+
+# image 1
+# bounds = [(123, 453), (428, 321), (621, 421), (302, 664)]
+
+# image 2
+bounds = [(0, 60), (500, 40), (620, 620), (-80, 560)]
+
+# image 3
+# bounds = [(120, 60), (520, 180), (520, 480), (120, 520)]
+
+
+matrix = cv2.getPerspectiveTransform(np.float32(bounds), np.float32([[0, 0], [dim[0], 0], dim, [0, dim[1]]]))
+
+transformed_image = cv2.warpPerspective(img, matrix, dim)
+
+colorImg = transformed_image.copy()
+colorImg = cv2.cvtColor(colorImg, cv2.COLOR_RGB2HSV)
+colorImg = cv2.inRange(colorImg, lowHSV, highHSV)
+
+
+edges = cv2.Canny(img,100,200)
+
+edges = cv2.warpPerspective(edges, matrix, dim)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+
+imgRed = cv2.dilate(edges, kernel, iterations=3)
+imgRed = cv2.morphologyEx(imgRed, cv2.MORPH_OPEN,
+                           kernel,
+                           iterations=1)
+
+contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+contours = [contour for contour in contours if cv2.contourArea(contour) > CONTOUR_THRESH_LOW and cv2.contourArea(contour) < CONTOUR_THRESH_HIGH]
+
+satMask = transformed_image.copy()
+satMask = cv2.cvtColor(satMask, cv2.COLOR_RGB2HSV)
+satMask = cv2.inRange(satMask, np.array([0, 110, 0]), np.array([255, 255, 255]), satMask)
+
+# contours.sort(key=cv2.contourArea)
+
+samples = contours
+
+# for contour in contours:
+#     mask = np.zeros(transformed_image.shape[:2], np.uint8)
+#     cv2.drawContours(mask, [np.intp(cv2.boxPoints(cv2.minAreaRect(contour)))], -1, 255, cv2.FILLED)
+
+#     maskedImg = cv2.bitwise_and(colorImg, colorImg, mask=mask)
+
+#     vals = cv2.mean(maskedImg)
+
+#     area = cv2.contourArea(contour)
+
+#     satVal = vals[0] * transformed_image.shape[0] * transformed_image.shape[1] / area
+
+#     satVal /= 255.0
+
+#     # print(satVal)
+
+#     if (satVal > 0.5):
+#         samples.append(contour)
+
+#         # remove from satmask
+#         cv2.bitwise_not(mask, mask)
+#         satMask = cv2.bitwise_and(satMask, satMask, mask=mask)
+
+
+def filterByRatio(sample) -> bool:
+    width = np.linalg.norm(sample[0] - sample[1])
+    length = np.linalg.norm(sample[1] - sample[2])
+
+    ratio = length / width
+    ratioInverted = width / length
+
+    area = length * width
+
+    MIN_AREA = 500
+    MAX_AREA = 4000
+    
+    if (area < MIN_AREA) or (area > MAX_AREA):
+        return False
+
+    MIN_RATIO = 1.3
+    MAX_RATIO = 1000
+
+    if ((ratio > MIN_RATIO) and (ratio < MAX_RATIO)) or ((ratioInverted > MIN_RATIO) and (ratioInverted < MAX_RATIO)):
+        return True
+    return False
+
+samples = list(map(lambda x: np.int0(cv2.boxPoints(cv2.minAreaRect(x))), samples))
+samples = list(filter(filterByRatio, samples))
+
+
+
+# print(samples)
+
+cv2.drawContours(transformed_image, samples, -1, color=(0, 255, 0), thickness=2)
+
+cv2.polylines(img, [np.int32(bounds)], True, (255, 0, 0), 5)
+
+
+# display image
+cv2.imshow("main", img)
+cv2.imshow("edges", edges)
+cv2.imshow("sat", satMask)
+cv2.imshow("done", transformed_image)
+cv2.imshow("yellow", colorImg)
+
+cv2.waitKey(0)
--- WilliamRithm/Hough.py
+++ WilliamRithm/Hough.py
@@ -2,66 +2,90 @@ import cv2
 import numpy as np
 import math
 
-# Algorithm details:
-# 1. Undistort camera
-# 2. Map perspective transform
-# 3. Convert to hsv and then threshold for red, blue, and yellow
-# 4. Find Contours
-# 5. Filter based on aspect ratio and minimum area
-# 6. Draw contours if needed
+#HSV constants
+# --- BLUE CONSTANTS ---
+blueLow = np.array([100, 60, 60])
+blueHigh = np.array([140, 255, 255])
 
-img = cv2.imread("img3.jpg")
+# --- RED CONSTANTS ---
+redTopLow = np.array([170, 70, 50])
+redTopHigh = np.array([180, 255, 255])
+
+redBottomLow = np.array([0, 70, 50])
+redBottomHigh = np.array([10, 255, 255])
+
+# --- YELLOW CONSTANTS ---
+yellowLow = np.array([20, 80, 80])
+yellowHigh = np.array([35, 255, 255])
+
+# Algorithm details:
+# 1.
+
+img = cv2.imread("imgs/img0.png")
 
 dim = (640, 480)
-#img.resize(dim)
 img = cv2.resize(img, dim)
 
 # Distortion stuff is handled by logitech here
+# Automatically undistorted, so there's no need for us to do that
 
 # Perspective transform
 # bounds = [(140, 48), (500, 46), (710, 360),  (0, 350)]
 bounds = [(200, 48), (500, 46), (630, 360),  (60, 330)]
 
-# for bound in bounds:
-  # cv2.circle(img, bound, 2, (255, 0, 0), 2)
-
-#cv2.polylines(img, [np.int32(bounds)], True, (255, 0, 0), 5)
-
-# cv2.polylines(img, [np.int32(bounds)], True, (0, 0, 0), 3)
-cv2.waitKey(0)
-
 matrix = cv2.getPerspectiveTransform(np.float32(bounds), np.float32([[0, 0], [dim[0], 0], dim, [0, dim[1]]]))
 transformed_image = cv2.warpPerspective(img, matrix, dim)
 
-# kernel = np.array([[0, 1, 1, 1, 0],
-#                   [1, 1, 1, 1, 1],
-#                   [1, 1, 1, 1, 1],
-#                   [1, 1, 1, 1, 0],
-#                   [0, 1, 1, 1, 1]], dtype=np.int32)
+# Can erode here if we want, makes contours worse so we don't do it
 # element = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2), (-1, -1))
-# transformed_image = cv2.erode(transformed_image, element)
-# transformed_image = cv2.dilate(transformed_image, element)
 # transformed_image = cv2.morphologyEx(transformed_image, cv2.MORPH_OPEN, element)
 
+#Hough is hough lines on the transformed image, raw_hough is empty mask for hough
 hough = transformed_image.copy()
-# raw_hough = np.zeros([transformed_image.shape[0], transformed_image.shape[1]], np.int8)
 raw_hough = np.zeros(transformed_image.shape[:2], np.uint8)
 
-#Erosion/Dilation
-element = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5), (-1, -1))
-transformed_image = cv2.erode(transformed_image, element)
-transformed_image = cv2.dilate(transformed_image, element)
+hsv_image = cv2.cvtColor(transformed_image, cv2.COLOR_BGR2HSV)
+# Threshold for HSV
+red1_threshold = cv2.inRange(hsv_image, redTopLow, redTopHigh)
+red2_threshold = cv2.inRange(hsv_image, redBottomLow, redBottomHigh)
+blue_threshold = cv2.inRange(hsv_image, blueLow, blueHigh)
+yellow_threshold = cv2.inRange(hsv_image, yellowLow, yellowHigh)
 
-#Blur -> For smoothing
-for i in range(10):
+CONTOUR_THRESH_LOW = 500
+CONTOUR_THRESH_HIGH = 10000
+
+# Find contours in masks
+r1c, _ = cv2.findContours(red1_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+r2c, _ = cv2.findContours(red2_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+bc, _ = cv2.findContours(blue_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+yc, _ = cv2.findContours(yellow_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+contours = r1c + r2c + bc + yc
+
+# Build mask of zeros (Bitwise AND is matrix multiplication, so anything 0 stays 0)
+mask = np.zeros(transformed_image.shape[:2], np.uint8)
+cv2.drawContours(mask, contours, -1, 255, thickness=cv2.FILLED)
+
+# Erode the contours because of noise
+element = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10), (-1, -1))
+mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, element)
+
+#Blur -> For smoothing, helps with canny edge detection. Do it 15 times and it makes it worse though
+for i in range(5):
   transformed_image = cv2.GaussianBlur(transformed_image,(5,5),0)
+#Find Edges
+edges = cv2.Canny(transformed_image, 50, 70)
 
-edges = cv2.Canny(transformed_image, 50, 100)
+# Use the HSV mask to clarify canny
+# This is not working right now
+cv2.bitwise_and(edges, mask)
 
-cv2.imshow("edges", edges)
+cv2.imshow("contours/mask", mask)
+cv2.imshow("masked edges", edges)
 
+#Build hough lines
 pixels_for_line = 10
-linesP = cv2.HoughLinesP(edges, 1, 2 * np.pi/180, pixels_for_line, None, 10, 15)
+linesP = cv2.HoughLinesP(edges, 1, 1 * np.pi/180, pixels_for_line, None, 10, 15)
 
 # Draw the lines
 if linesP is not None:
@@ -70,10 +94,15 @@ if linesP is not None:
         cv2.line(hough, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
         cv2.line(raw_hough, (l[0], l[1]), (l[2], l[3]), 255, 10, cv2.LINE_AA)        
 
+cv2.bitwise_and(raw_hough, mask)
+
 hough_contours, _ = cv2.findContours( 
     raw_hough, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) 
 
+# SHAPE DETECTION
+
 for contour in hough_contours:
+  # Skip first thing cuz it just works like that
   if i == 0: 
     i = 1
     continue
@@ -92,6 +121,7 @@ for contour in hough_contours:
       x = int(M['m10']/M['m00']) 
       y = int(M['m01']/M['m00']) 
 
+  # shape detection
   # if len(approx) == 4: 
   #     cv2.putText(raw_hough, 'Quadrilateral', (x, y), 
   #                 cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2) 
@@ -110,47 +140,13 @@ for contour in hough_contours:
       
   cv2.circle(raw_hough, (x, y), 3, (255, 0, 0), 1)
 
+for bound in bounds:
+  cv2.circle(img, bound, 2, (255, 0, 0), 2)
+
+cv2.polylines(img, [np.int32(bounds)], True, (255, 0, 0), 5)
+
 cv2.imshow("hough", hough)
 cv2.imshow("raw hough", raw_hough)
-cv2.imshow("transformed", transformed_image)
+
+cv2.imshow("raw", img)
 cv2.waitKey(0)
-
-#Standard Blue Yellow Red Contour Detection Below
-
-# # --- BLUE CONSTANTS ---
-# blueLow = np.array([0, 60, 30])
-# blueHigh = np.array([80, 255, 255])
-
-# # --- RED CONSTANTS ---
-# redLow = np.array([115, 60, 80])
-# redHigh = np.array([180, 255, 2])
-
-# # --- YELLOW CONSTANTS ---
-# yellowLow = np.array([20, 80, 80])
-# yellowHigh = np.array([30, 255, 255])
-
-# hsv_image = cv2.cvtColor(transformed_image, cv2.COLOR_RGB2HSV)
-# # Make masks
-# red_threshold = cv2.inRange(hsv_image, redLow, redHigh)
-# blue_threshold = cv2.inRange(hsv_image, blueLow, blueHigh)
-# yellow_threshold = cv2.inRange(hsv_image, yellowLow, yellowHigh)
-
-# CONTOUR_THRESH_LOW = 500
-# CONTOUR_THRESH_HIGH = 4000
-
-# rc, _ = cv2.findContours(red_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# bc, _ = cv2.findContours(blue_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# yc, _ = cv2.findContours(yellow_threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-# #contours = [contour for contour in contours if cv2.contourArea(contour) > CONTOUR_THRESH_LOW and cv2.contourArea(contour) < CONTOUR_THRESH_HIGH]
-
-# cv2.drawContours(transformed_image, bc, -1, color=(0, 255, 0), thickness=5)
-
-# for bound in bounds:
-#   cv2.circle(img, bound, 2, (255, 0, 0), 2)
-
-# cv2.polylines(img, [np.int32(bounds)], True, (255, 0, 0), 5)
-
-# cv2.imshow("raw", img)
-# cv2.imshow("transformed", transformed_image)
-# cv2.waitKey(0)
